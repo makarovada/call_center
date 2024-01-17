5b4bcdec9ca08c1c3f0d346c8e64c1c78425ea9c
Дарья <makarovada.12345@gmail.com>
2024-01-17 03:50:19 +0300

    Отображение кластеров

--- cluster.php
+++ cluster.php
@@ -9,16 +9,24 @@ require('db.php');
 <?php if ($_GET["type"]=='cluster'):?>
 <div class="row g-3">
 
-    <label for="exampleSelect1" class="form-label mt-4">Кластер</label>
     <div class="col-sm-4">
-        <select class="form-select" id="exampleSelect1">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-        </select>
-    </div>
+    <div class="btn-group">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle='dropdown'>Номер кластера<span class='caret'></span></button>
+        <ul class='dropdown-menu'>
+            <?php
+            $typeQuery = "SELECT DISTINCT label FROM appeals WHERE label<>0 ORDER BY label";
+            $result = $connect->query($typeQuery);
+
+            if ($result && $result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<li><a class='dropdown-item' href='?label=" .$row['label']. "&type=cluster'>" . $row['label'] . "</a></li>";
+                }
+            }
+            $label = isset($_GET['label']) ? $_GET['label'] : 0;
+
+            
+            ?>
+        </ul></div></div>
     <div class="col-sm-8">
         <div class="input-group mb-3">
         <input type="text" class="form-control" placeholder="Название кластера" aria-label="Название кластера" aria-describedby="button-addon2">
@@ -26,6 +34,8 @@ require('db.php');
         </div>
     </div>
 </div>
+</div>
+
 <?php endif;?>
 <table class="table table-hover">
   <thead>
@@ -35,22 +45,37 @@ require('db.php');
       <th scope="col">Голосовое меню</th>
       <th scope="col">Нахождение в очереди</th>
       <th scope="col">Общение с оператором</th>
-      <th scope="col">Постобработка</th>
+      <th class="table-primary" scope="col">Постобработка</th>
       <th scope="col">Тематика обращения</th>
-      <th scope="col">Основная тема</th>
+      <th class="table-primary" scope="col">Основная тема</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">Active</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column contentColumn contentColumn contentColumn contentColumn contentColumn contentColumn contentColumn contentColumn content</td>
-      <td>Column contentColumn contentColumn contentColumn contentColumn contentColumn contentColumn contentColumn contentColumn content</td>
-    </tr>
+    <?php
+    if ($label <> 0) {
+        $query = "SELECT * FROM (SELECT * FROM appeals WHERE label=?) AS a JOIN dictionary ON appeal_num=id ORDER BY postProcessingTime, appeal_num";
+    
+        $stmt = $connect->prepare($query);
+        $stmt->bind_param('i', $label);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<th scope='row'>".$row["id_appeal"]."</th>";
+                echo "<td>".$row["dateTime"]."</td>";
+                echo "<td>".$row["voiceMenu"]."</td>";
+                echo "<td>".$row["queueTime"]."</td>";
+                echo "<td>".$row["withOperatorTime"]."</td>";
+                echo "<td class='table-primary'>".$row["postProcessingTime"]."</td>";
+                echo "<td>".$row["appeal"]."</td>";
+                echo "<td class='table-primary'>".$row["theme"]."</td>";
+                echo "</tr>";
+            }
+        } 
+    }
+    ?>
   </tbody>
 </table>
 <!-- для аномалий -->
--- header.php
+++ header.php
@@ -4,7 +4,8 @@
       <title><?=$title?></title>
       <meta charset="utf-8" />
       <link rel="stylesheet" href="bootstrap.min.css" />
-      <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-KGDJBEFF3W"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
   </head>
   <body>
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
